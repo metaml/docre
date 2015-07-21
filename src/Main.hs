81b8eb0e9d2a5fd7972d5c97e2f5a3bd67f3aa72
ml <metaml@gmail.com>
2015-07-21 01:49:13 -0400

    @todo: bug fix|handle non 200 http responses from docker.sock

--- src/Main.hs
+++ src/Main.hs
@@ -7,17 +7,19 @@ import Control.Exception (try, throwIO)
 import Control.Monad (unless, forever)
 import Network.Socket hiding (recv)
 import Network.Socket.ByteString (recv, sendAll)
-import Data.Aeson as Aeson
+import Data.Aeson (decodeStrict)
+import Data.Aeson.Types
 import Data.Maybe (fromMaybe, fromJust)
 import Data.ByteString (concat, length, drop, null, pack, ByteString)
 import Data.ByteString.Char8 (putStrLn, breakSubstring)
+import qualified Data.Text as Text
 import Data.Text.Encoding (encodeUtf8)
 import qualified Data.HashMap.Strict as HM
 import Pipes ((>->), await, yield, runEffect, lift, Consumer, Producer, Pipe)
 import Data.Docker (Event(..))
 
 main :: IO ()
-main = runEffect $ docker >-> json2event >-> event2Id >-> id2container >-> container2consul >-> consul
+main = runEffect $ docker >-> json2event >-> event2id >-> id2container >-> container2consul >-> consul
 
 -- | @todo: handle error cases
 docker :: Producer ByteString IO ()
@@ -29,46 +31,48 @@ docker = forever $ do
 json2event :: Pipe ByteString Event IO ()
 json2event = forever $ do
                r <- await
+               lift $ putStrLn "- json2event:" >> print r
                let j = last (tokenize "\r\n" r)
-               case (Aeson.decodeStrict j) of
+               case (decodeStrict j) of
                  Nothing -> lift $ putStrLn "error in json2event:" >> print j
                  Just e -> yield e
 
-event2Id :: Pipe Event ByteString IO ()
-event2Id = forever $ do
+event2id :: Pipe Event (ByteString, ByteString) IO ()
+event2id = forever $ do
              e <- await
-             yield $ encodeUtf8 $ eventId e 
+             yield $ (encodeUtf8 $ eventId e, encodeUtf8 $ eventStatus e)
 
-id2container :: Pipe ByteString ByteString IO ()
+id2container :: Pipe (ByteString, ByteString) (ByteString, ByteString) IO ()
 id2container = forever $ do
                  s <- lift $ unixSocket
                  _ <- ($) forever $ do
-                           eId <- await
+                           (eId, status) <- await
                            lift $ sendAll s $ concat ["GET /containers/", eId, "/json HTTP/1.1", "\r\n\r\n"]
                            r <- lift $ recv s 4096
-                           yield $ last (tokenize "\r\n" r)
+                           lift $ putStrLn "- id2container:" >> print r
+                           yield $ (r, status)
                  lift $ sClose s
 
-container2consul :: Pipe ByteString Object IO ()
+container2consul :: Pipe (ByteString, ByteString) (Object, ByteString) IO ()
 container2consul = forever $ do
-                     j <- await
-                     let o :: Maybe Object = Aeson.decodeStrict j
+                     (r, status) <- await
+                     let o :: Maybe Object = decodeStrict (last $ tokenize "\r\n" r)
                      case o of
                        Nothing -> lift $ putStrLn "error in container2consul"
-                       Just e -> yield e
-
-consul :: Consumer Object IO ()
+                       Just e -> yield (e, status)
+                                 
+consul :: Consumer (Object, ByteString) IO ()
 consul = forever $ do
-           h  <- await
-           let did = fromMaybe "" (HM.lookup "Id" h)
-               name = fromMaybe "" (HM.lookup "Name" h)
-           lift $ print did
-           lift $ print name
-           lift $ print $ HM.lookup "NetworkSettings" h
+           (h, status) <- await
+           let (Just (String did)) = HM.lookup "Id" h
+               (Just (String name)) = HM.lookup "Name" h
+               Just net = HM.lookup "NetworkSettings" h
+               (Just (String ip)) = ipAddress net
+           lift $ print status >> print name >> print ip >> print did >> putStrLn "##########################"
 
 event :: Socket -> IO ByteString
 event s = do
-  sendAll s "GET /events HTTP/1.1\r\n\r\n"
+  sendAll s "GET /events HTTP/1.1\r\nContent-Type: application/json\r\n\r\n"
   j <- recv s 4096
   return j
 
@@ -76,13 +80,17 @@ unixSocket :: IO Socket
 unixSocket = do
   s <- socket AF_UNIX Stream defaultProtocol
   connect s $ SockAddrUnix "/var/run/docker.sock"
-  return s     
-         
+  return s
+
+ipAddress :: Value -> Maybe Value
+ipAddress (Object o) = HM.lookup "IPAddress" o
+ipAddress _ = Nothing
+
+-- @todo: refactor
 tokenize :: ByteString -> ByteString -> [ByteString]
 tokenize d bs = filter (\e -> e /= "") $ h : if null t then [] else tokenize d (drop (length h) t)
                                              where (h, t) = breakSubstring d bs
 
-                                                            
 -- | below: only for reference
 consul' :: Consumer (Maybe Event) IO ()
 consul' = do
